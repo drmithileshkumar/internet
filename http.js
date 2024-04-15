17b2f0b8a9a3186dc97bdcd10fffa38802a94d06
Mithilesh Kumar <admin@drmithileshkumar.com>
2024-04-15 13:05:09 +0530

    data

--- http.js
+++ http.js
@@ -1,5 +1,6 @@
 let nameElement = document.querySelector('.name');
 let surnameElement = document.querySelector('.surname')
+let surnameInput = document.querySelector('#surnameInput');
 
 const getMyName = async () => {
     let res = await fetch('http://127.0.0.1:3000/')
@@ -12,14 +13,15 @@ const getMyName = async () => {
     nameElement.textContent = name;
 }
 const postSurname = async () => {
+    let mysurname = surnameInput.value;
     let res = await fetch('http://127.0.0.1:3000/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({surname: 'Kumar'})
+        body: JSON.stringify({surname: mysurname})
     })
-    console.log(res);
+    // console.log(res);
     if(!res.ok) {
         alert('Failed to post surname');
         return;
--- server.mjs
+++ server.mjs
@@ -3,7 +3,7 @@ import { createServer } from 'node:http';
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = createServer((req, res) => {
+const server = createServer( async (req, res) => {
   console.log('Received request...', req.url, req.method);
   
   if(req.url == '/') {
@@ -25,15 +25,25 @@ const server = createServer((req, res) => {
       res.end('OK');
     }
     if(req.method == 'POST') {
-      // console.log(req.body());
-      console.log('post method...')
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.setHeader('Access-Control-Allow-Origin', "*");
-
-
-      res.end(JSON.stringify({surname: "KUMAR"}));
-      // console.log(res)
+      let data = '';
+      req.on('data', chunk => {
+        data += chunk;
+      });
+      req.on('end', () => {
+        try {
+          const userData = JSON.parse(data);
+          const {surname} = userData;
+          console.log('Surname...', surname);
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          res.setHeader('Access-Control-Allow-Origin', "*");
+          res.end(JSON.stringify({surname}));
+        } catch (e) {
+          console.error('Error parsing JSON: ', e.message);
+          res.writeHead(400, { 'Content-Type': 'text/plain'});
+          res.end('Invalid JSON data');
+        }
+      })
     }
   }
   
